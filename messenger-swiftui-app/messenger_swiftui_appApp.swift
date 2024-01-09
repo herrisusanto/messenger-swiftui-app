b621e558bd11b9182f0f52d8e53c529cfc96d97f
Heri Susanto <heri.susanto@neuronsquare.com>
2024-01-09 14:19:47 +0700

    Initial Commit

---
+++ messenger-swiftui-app/messenger_swiftui_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  messenger_swiftui_appApp.swift
+//  messenger-swiftui-app
+//
+//  Created by loratech on 09/01/24.
+//
+
+import SwiftUI
+
+@main
+struct messenger_swiftui_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
