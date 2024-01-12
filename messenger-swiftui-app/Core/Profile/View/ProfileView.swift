9c703c3808ae5ac987d71556af7e23c32e07b2ed
Heri Susanto <heri.susanto@neuronsquare.com>
2024-01-12 20:39:17 +0700

    Add Profile View Model and Integrate PhotosUI to pick image from directory

--- messenger-swiftui-app/Core/Profile/View/ProfileView.swift
+++ messenger-swiftui-app/Core/Profile/View/ProfileView.swift
@@ -6,15 +6,29 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 struct ProfileView: View {
+    @StateObject var viewModel = ProfileViewModel()
     var body: some View {
         VStack {
             VStack {
-                Image(systemName: "person.circle.fill")
-                    .resizable()
-                    .frame(width: 80, height: 80)
-                    .foregroundStyle(Color(.systemGray4))
+                PhotosPicker(selection: $viewModel.selectedItem) {
+                    if let profileImage = viewModel.profileImage {
+                        profileImage
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 80, height: 80)
+                            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                    } else {
+                        
+                        Image(systemName: "person.circle.fill")
+                            .resizable()
+                            .frame(width: 80, height: 80)
+                            .foregroundStyle(Color(.systemGray4))
+                    }
+                }
+                
                 Text("Black Panther")
                     .font(.title2)
                     .fontWeight(.semibold)
---
+++ messenger-swiftui-app/Core/Profile/ViewModel/ProfileViewModel.swift
@@ -0,0 +1,28 @@
+//
+//  ProfileViewModel.swift
+//  messenger-swiftui-app
+//
+//  Created by loratech on 12/01/24.
+//
+
+import SwiftUI
+import PhotosUI
+
+class ProfileViewModel: ObservableObject {
+    @Published var selectedItem: PhotosPickerItem? {
+        didSet {
+            Task {
+                try await loadImage()
+            }
+        }
+    }
+    
+    @Published var profileImage: Image?
+    
+    func loadImage () async throws {
+        guard let item = selectedItem else {return}
+        guard let imageData = try await item.loadTransferable(type: Data.self) else {return}
+        guard let uiImage = UIImage(data: imageData) else {return}
+        self.profileImage = Image(uiImage: uiImage)
+    }
+}
